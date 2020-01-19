ebeb6ea68fdd8ca2b779a8282f0f1c0e47c9f7d9
Andres Alonso <andres@Andress-MBP.fios-router.home>
2020-01-19 10:25:09 -0800

    Updates

---
+++ precache-manifest.bac6c8bb73763375875b4e129d17b4ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e84b06b168a49a6b2802e4897eda7c",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "018aa5c56c6d52e422e0",
+    "url": "/tetris/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "082978c4261688c4d2c9",
+    "url": "/tetris/static/js/2.4ea52300.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tetris/static/js/2.4ea52300.chunk.js.LICENSE"
+  },
+  {
+    "revision": "018aa5c56c6d52e422e0",
+    "url": "/tetris/static/js/main.52eb8569.chunk.js"
+  },
+  {
+    "revision": "7299ff2b414667a6e0ff",
+    "url": "/tetris/static/js/runtime-main.831c2a87.js"
+  }
+]);
